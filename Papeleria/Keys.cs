7f3703a87b213386f17271d5996c6cedcee70b6b
Unknown <daniel.tejedor@outlook.com>
2019-05-21 09:47:21 +0200

    Prepared Keys for secrets

--- Papeleria/Keys.cs
+++ Papeleria/Keys.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Papeleria
 {
-    partial class Keys
+    partial class AnyadirLibro
     {
+        private const string KEYProds = "";
+        private const string KEYStocks = "";
+        private const string KEYFeaturesValues = "";
+        private const string KEYCategories = "";
+        private const string KEYImages = "";
+        //private const string KEYFeatures = "";
+        //private const string KEYCustomers = "";
 
     }
 }
